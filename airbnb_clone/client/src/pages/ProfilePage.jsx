5399e6e10e46ec25c0e78c84682730c6230ae229
hhuynh10 <101972803+hhuynh10@users.noreply.github.com>
2023-03-23 19:55:26 -0500

    projects

--- airbnb_clone/client/src/pages/ProfilePage.jsx
+++ airbnb_clone/client/src/pages/ProfilePage.jsx
@@ -1,9 +1,23 @@
 import {useContext, useState} from "react";
 import {UserContext} from "../UserContext.jsx";
+import {Link, Navigate, useParams} from "react-router-dom";
+import axios from "axios";
+import PlacesPage from "./PlacesPage";
+import AccountNav from "../AccountNav";
 
-const ProfilePage = () => {
+export default function ProfilePage() {
     const [redirect,setRedirect] = useState(null);
     const {ready,user,setUser} = useContext(UserContext);
+    let {subpage} = useParams();
+    if (subpage === undefined) {
+        subpage = 'profile';
+    }
+
+    async function logout() {
+        await axios.post('/api/logout');
+        setRedirect('/');
+        setUser(null);
+    }
 
     if (!ready) {
         return 'Loading...';
@@ -12,20 +26,22 @@ const ProfilePage = () => {
     if (ready && !user && !redirect) {
         return <Navigate to={'/login'} />
     }
-    
+
     if (redirect) {
-    return <Navigate to={redirect} />
+        return <Navigate to={redirect} />
     }
-
-    async function logout() {
-        await axios.post('/logout');
-        setRedirect('/');
-        setUser(null);
-    }
-
     return (
-        <div>account page for {user.name}</div>
-    )
+        <div>
+        <AccountNav />
+        {subpage === 'profile' && (
+            <div className="text-center max-w-lg mx-auto">
+            Logged in as {user.name} ({user.email})<br />
+            <button onClick={logout} className="primary max-w-sm mt-2">Logout</button>
+            </div>
+        )}
+        {subpage === 'places' && (
+            <PlacesPage />
+        )}
+        </div>
+    );
 }
-
-export default ProfilePage
